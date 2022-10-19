4853a5fe600badd2520c6c8f9ad50f94ac167d47
Christiano300 <christian.patzl@gmx.net>
2022-10-19 15:05:29 +0200

    first commit

---
+++ src/schaltjahr/Schaltjahr.java
@@ -0,0 +1,7 @@
+package schaltjahr;
+
+public class Schaltjahr {
+    public static boolean istSchaltjahr(int jahr) {
+        return false;
+    }
+}
---
+++ tests/schaltjahr/SchaltjahrTest.java
@@ -0,0 +1,32 @@
+package schaltjahr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SchaltjahrTest {
+    @Test
+    public void testSchaltjahr_2000() {
+        assertTrue(Schaltjahr.istSchaltjahr(2000));
+    }
+    @Test
+    public void testSchaltjahr_2001() {
+        assertFalse(Schaltjahr.istSchaltjahr(2001));
+    }
+    @Test
+    public void testSchaltjahr_1900() {
+        assertFalse(Schaltjahr.istSchaltjahr(1900));
+    }
+    @Test
+    public void testSchaltjahr_2004() {
+        assertTrue(Schaltjahr.istSchaltjahr(2004));
+    }
+    @Test
+    public void testSchaltjahr_2008() {
+        assertTrue(Schaltjahr.istSchaltjahr(2008));
+    }
+    @Test
+    public void testSchaltjahr_2010() {
+        assertFalse(Schaltjahr.istSchaltjahr(2010));
+    }
+}
