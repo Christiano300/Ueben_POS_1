7ca6fa11bc1f8bde2edbd8a8d7a9ceb2dccfa956
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 20:28:29 +0200

    Kommentare

--- src/ArrayUtil.java
+++ src/ArrayUtil.java
@@ -1,7 +1,14 @@
 public class ArrayUtil {
+    /*
+    Setzt im array beim gegebenen index die Zahl ein, bei ungültigem indes gibt es null zurück
+     */
     public static int[] insertAt(int[] array, int index, int zahl) {
         return array;
     }
+
+    /*
+    Löscht im gegebenen Array die Zahl beim gegebenen index
+     */
     public static int[] deleteAt(int[] array, int index) {
         return array;
     }
--- src/Datum.java
+++ src/Datum.java
@@ -1,8 +1,14 @@
 public class Datum {
+    /*
+    Gibt zurück ob das gegebene Jahr ein Schaltjahr ist
+     */
     public static boolean istSchaltjahr(int jahr) {
         return false;
     }
 
+    /*
+    Gibt zurück ob das gegebene Datum gültig ist
+     */
     public static boolean istDatumGueltig(int tag, int monat, int jahr) {
         return false;
     }
